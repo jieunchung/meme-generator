897a79f14397b912bbdf942877d1d1b26fd94e5e
jieunchung <106100696+jieunchung@users.noreply.github.com>
2022-10-08 10:36:41 +0200

    Display using API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Meme = () => {
   const shadow = {
     textShadow: "2px 2px 4px #000000",
   };
 
-  /* const [allMemes, setAllMemes] = useState([]) */
+  const [allMemes, setAllMemes] = useState([]);
+
   const [meme, setMeme] = useState({
     text1: "",
     text2: "",
     randomImage: "https://i.imgflip.com/tau4.jpg",
   });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setMeme((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((result) => setAllMemes(result));
+  }, []);
+
+  const getMemeImage = () => {
+    const memeArray = allMemes.data.memes
+      .filter((meme) => meme.box_count === 2)
+      .map((filteredMeme) => filteredMeme);
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const imageUrl = memeArray[randomNumber].url;
+
+    setMeme((prev) => ({
+      ...prev,
+      randomImage: imageUrl,
+    }));
+  };
 
   return (
     /* meme */
@@ -23,6 +51,7 @@ const Meme = () => {
           placeholder="text1"
           name="text1"
           value={meme.text1}
+          onChange={handleChange}
         ></input>
         <input
           className="border-2 p-3 border-gray-500 rounded-lg text-xs"
@@ -30,11 +59,15 @@ const Meme = () => {
           placeholder="text2"
           name="text2"
           value={meme.text2}
+          onChange={handleChange}
         ></input>
       </div>
       {/* button */}
       <div className="grid grid-cols-1">
-        <button className="border-2 border-gray-500 rounded-lg w-full p-3 text-sm">
+        <button
+          className="border-2 border-gray-500 rounded-lg w-full p-3 text-sm"
+          onClick={getMemeImage}
+        >
           get more memes✌️
         </button>
       </div>
@@ -42,15 +75,15 @@ const Meme = () => {
       <div className="relative max-w-full">
         <img className="max-w-full min-h-full" src={meme.randomImage} />
         <h2
+          style={shadow}
           className="absolute w-full text-center top-2 text-xl sm:text-4xl font-bold text-white uppercase"
-          styles={shadow}
         >
           {meme.text1}
         </h2>
         <h2
+          style={shadow}
           className="absolute w-full text-center bottom-2 text-xl sm:text-4xl font-bold text-white uppercase
         "
-          styles={shadow}
         >
           {meme.text2}
         </h2>
